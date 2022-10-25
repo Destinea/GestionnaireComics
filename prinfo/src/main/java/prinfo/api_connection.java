678da01e96e7809cacc1cdcea5df276720d4c006
BIER <cyril.bier42100@gmail.com>
2022-10-25 11:55:39 +0200

    modif 25/10/22

--- prinfo/src/main/java/prinfo/api_connection.java
+++ prinfo/src/main/java/prinfo/api_connection.java
@@ -11,7 +11,15 @@ public class api_connection {
     private static final String USER_AGENT = "Mozilla/5.0";
     private static final String API_KEY = "53b33e3da09b63c64d3a69667f455e9076055dcc";
 
+    /**
+     * Permet de rechercher des infos sur la bdd comicvine.
+     * @param recherche L'info que l'on recherche.
+     * @return Resultat de la requête sous format xml.
+     * @author Cyril
+     * @throws IOException
+     */
     public String Recherche_API(String recherche) throws IOException {
+        //TODO Ajouter la clé API dans un header pas dans la requête
         String requeteURL = "https://comicvine.gamespot.com/api/search/?query=" + recherche + "&api_key=" + API_KEY;
         String reponse;
 
@@ -33,4 +41,11 @@ public class api_connection {
 
         return reponse;
     }
+    /*
+    public static void main(String[] args) throws IOException {
+        api_connection test = new api_connection();
+        String resultat = test.Recherche_API("wolverine");
+        System.out.println(resultat);
+    }
+    */
 }
