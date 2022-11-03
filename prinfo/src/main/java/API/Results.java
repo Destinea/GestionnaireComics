2db87d8ee23ab8a4e785045cea253b8b5a53a4ea
bier.cyril <cyril.bier42100@gmail.com>
2022-11-03 14:05:59 +0100

    Création de la classe Results + adaptation de la classe api_connection pour fonctionner avec results

--- prinfo/src/main/java/API/Results.java
+++ prinfo/src/main/java/API/Results.java
@@ -1,2 +1,44 @@
-package API;public class Results {
+package API;
+
+/**
+ * Classe permettant d'instancier le résultat d'une recherche
+ * @author Cyril
+ */
+public class Results {
+    private String name;
+    private String shortDescription;
+    private String type;
+    private String publisher;
+    private int id;
+    private String iconLink;
+
+
+    public Results(String name, String shortDescription, String type, String publisher, int id, String iconLink) {
+        this.name = name;
+        this.shortDescription = shortDescription;
+        this.type = type;
+        this.publisher = publisher;
+        this.id = id;
+        this.iconLink = iconLink;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
--- prinfo/src/main/java/API/api_connection.java
+++ prinfo/src/main/java/API/api_connection.java
@@ -1,4 +1,4 @@
-package prinfo;
+package API;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -8,20 +8,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Classe permettant d'effctuer des recherches sur ComicVine.
+ * @author Cyril
+ */
 public class api_connection {
-    //TODO : Gérer l'accès aux différentes pages (pour l'instant on récupère que la première page de résultats)
     private static final String USER_AGENT = "Mozilla/5.0";
     private static final String API_KEY = "53b33e3da09b63c64d3a69667f455e9076055dcc";
 
     /**
-     * Renvoie tout les resultats liés à l'entrée passée en paramètre.
-     * @param recherche L'information que l'on recherche.
+     * Renvoie le JSON lié à l'entrée passée en paramètre.
+     * @param recherche : Information que l'on recherche.
+     * @param numPage : numéro de la page que l'on recherche (10 recherche affichées par page.
      * @return Resultat de la requête sous format json.
      * @author Cyril
      */
-    private String getJsonResults(String recherche) throws IOException {
-        String requeteURL = "https://comicvine.gamespot.com/api/search/?query=" + recherche + "&format=json&api_key=" + API_KEY;
+    private String getJsonResults(String recherche, int numPage) throws IOException {
+        String requeteURL = "https://comicvine.gamespot.com/api/search/?query=" + recherche + "&page="+numPage+"&format=json&api_key=" + API_KEY;
         String reponse;
 
         URL obj = new URL(requeteURL);
@@ -45,22 +51,45 @@ public class api_connection {
 
     /**
      * Instancie les résultats de la recherche.
-     * @param recherche l'information que l'on recherche
-     * @return Un tableau contenant les objets associés à la requête
+     * @param recherche : Information que l'on recherche
+     * @param numPage : numéro de la page que l'on recherche (10 recherche affichées par page).
+     * @return Liste des résultats obtenus.
      * @author Cyril
      */
-    public JSONArray GetResults(String recherche) throws IOException {
-        String jsonString = getJsonResults(recherche);
+    public List<Results> GetResults(String recherche, int numPage) throws IOException {
+        List<Results> results = new ArrayList<>();
+
+        String jsonString = getJsonResults(recherche,numPage);
         JSONObject obj = new JSONObject(jsonString);
-        return obj.getJSONArray("results");
+        JSONArray JSONresults = obj.getJSONArray("results");
+        for(int i=0;i<JSONresults.length();i++){
+            String name = JSONresults.getJSONObject(i).getString("name");
+            String shortDescription = JSONresults.getJSONObject(i).get("deck").toString();
+            String type =JSONresults.getJSONObject(i).getString("resource_type");
+            String publisher = JSONresults.getJSONObject(i).getJSONObject("publisher").getString("name");
+            int id = JSONresults.getJSONObject(i).getInt("id");
+            String iconLink = JSONresults.getJSONObject(i).getJSONObject("image").getString("icon_url");
+            results.add(new Results(name,shortDescription,type,publisher,id, iconLink));
+        }
+        return results;
     }
 
 
-    /* Pour tester
+
+    /* Exemple d'utilisation de la classe :
     public static void main(String[] args) throws IOException {
         api_connection test = new api_connection();
-        JSONArray results = test.GetResults("wolverine");
+        List<Results> ResultatsRecherche = test.GetResults("wolverine",1);
+
+
+        for (Results results : ResultatsRecherche) {
+            System.out.println(results.getName() + "\n");
+            System.out.println(results.getPublisher() + "\n");
+            System.out.println(results.getShortDescription() + "\n");
+            System.out.println(results.getType() + "\n");
+            System.out.println("\n");
+        }
     }
-    */
+     */
 
 }
