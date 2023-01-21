a96c8b0945089908271a8d9d9cdcf27a5c26ad76
DESVIGNES NOUVEL <alexi.desvignesnouvel@gmail.com>
2023-01-21 16:59:23 +0100

    Ajout de l'icone que vous arretez pas d'enlever

--- prinfo/src/main/java/AffichagePrincipal/FenetrePrincipale.java
+++ prinfo/src/main/java/AffichagePrincipal/FenetrePrincipale.java
@@ -119,7 +119,7 @@ public class FenetrePrincipale extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-        String sImageFileName = "icon.png";
+        String sImageFileName = "../icon.png";
         URL urlImageFileName = getClass().getResource(sImageFileName);
 
         if (urlImageFileName == null)
