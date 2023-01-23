47fa8543a4881d772e27f3c95dd10517f5ecd37e
barranger-candas.justine <justine.barranger-candas@telecom-st-etienne.fr>
2023-01-23 23:18:26 +0100

    Ajout envoie du mail

--- prinfo/src/main/java/AffichageCompte/ConnectionFrame.java
+++ prinfo/src/main/java/AffichageCompte/ConnectionFrame.java
@@ -316,39 +316,11 @@ public class ConnectionFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jPasswordField1ActionPerformed
 
     private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseClicked
-        sendMail("justine.barranger.candas@etu.univ-st-etienne.fr");
+        MotDePasseOublie motDePasseOublier  = new MotDePasseOublie();
+        motDePasseOublier.setLocationRelativeTo(null);
+        motDePasseOublier.setVisible(true);
     }//GEN-LAST:event_jLabel1MouseClicked
     
-    public static void sendMail(String recipientEmail) {
-    Properties properties = new Properties();
-    properties.put("mail.smtp.auth", "true");
-    properties.put("mail.smtp.starttls.enable", "true");
-    properties.put("mail.smtp.host", "smtp.gmail.com");
-    properties.put("mail.smtp.port", "587");
-
-    String myAccountEmail = "netcomicstse@gmail.com";
-    String password = "cjkxmfeojnmsqopm";
-
-    Session session = Session.getInstance(properties, new Authenticator() {
-      @Override
-      protected PasswordAuthentication getPasswordAuthentication() {
-        return new PasswordAuthentication(myAccountEmail, password);
-      }
-    });
-
-    try {
-      Message message = new MimeMessage(session);
-      message.setFrom(new InternetAddress(myAccountEmail));
-      message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipientEmail));
-      message.setSubject("Test Email");
-      message.setText("Hello, this is a test email.");
-
-      Transport.send(message);
-      System.out.println("Email sent successfully to " + recipientEmail);
-    } catch (MessagingException e) {
-      e.printStackTrace();
-    }
-  }
     /**
      * @param args the command line arguments
      */
